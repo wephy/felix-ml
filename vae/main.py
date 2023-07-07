f7e5d2d479032ae272167535d8d12a8226c1a10a
Joe Webb <phusnq@toland.csc.warwick.ac.uk>
2023-07-07 17:47:01 +0100

    latent features changes

--- vae/main.py
+++ vae/main.py
@@ -5,99 +5,56 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
+import matplotlib.pyplot as plt
+import logging
 
 import importer  # noqa # pylint: disable=unused-import
 from datasets import FLAGDataset  # noqa # pylint: disable=import-error
+from model import CVAE
 
-# etup
+# ============= Hyperparams ==============
+batch_size = 64
+latent_size = 20
+epochs = 10
+image_size = 128 * 128
+seed = 1
+num_workers = 1
+
+# ================ Setup =================
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
-kwargs = {'num_workers': 1, 'pin_memory': True} 
+kwargs = {'num_workers': num_workers, 'pin_memory': True} 
+torch.manual_seed(seed)
 
-# hyper params
-batch_size = 64
-latent_size = 20
-epochs = 10
-
+# ============= Load dataset =============
 flag_location = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "datasets", "flag"))
 flag_dataset = FLAGDataset(flag_location)
-flag_shrunk = torch.utils.data.Subset(flag_dataset, torch.arange(11000))
-
-train_dataset, test_dataset = random_split(flag_shrunk, [10000, 1000])
+flag_12k = torch.utils.data.Subset(flag_dataset, torch.arange(12000))
+train_dataset, test_dataset = random_split(flag_12k, [10500, 1500])
 train_loader = DataLoader(
     dataset=train_dataset,  # the dataset instance
     batch_size=batch_size,  # automatic batching
     drop_last=True,  # drops the last incomplete batch in case the dataset size is not divisible by 64
     shuffle=True,  # shuffles the dataset before every epoch
-    num_workers=1,
+    **kwargs,
 )
 test_loader = DataLoader(
     dataset=test_dataset,  # the dataset instance
     batch_size=batch_size,  # automatic batching
     drop_last=True,  # drops the last incomplete batch in case the dataset size is not divisible by 64
     shuffle=True,  # shuffles the dataset before every epoch
-    num_workers=1,
+    **kwargs,
 )
 
-
-class CVAE(nn.Module):
-    def __init__(self, feature_size, latent_size, class_size):
-        super(CVAE, self).__init__()
-        self.feature_size = feature_size
-        self.class_size = class_size
-
-        # encode
-        self.fc1  = nn.Linear(feature_size + class_size, 400)
-        self.fc21 = nn.Linear(400, latent_size)
-        self.fc22 = nn.Linear(400, latent_size)
-
-        # decode
-        self.fc3 = nn.Linear(latent_size + class_size, 400)
-        self.fc4 = nn.Linear(400, feature_size)
-
-        self.elu = nn.ELU()
-        self.sigmoid = nn.Sigmoid()
-
-    def encode(self, x, c): # Q(z|x, c)
-        '''
-        x: (bs, feature_size)
-        c: (bs, class_size)
-        '''
-        inputs = torch.cat([x, c], 1) # (bs, feature_size+class_size)
-        h1 = self.elu(self.fc1(inputs))
-        z_mu = self.fc21(h1)
-        z_var = self.fc22(h1)
-        return z_mu, z_var
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
-
-    def decode(self, z, c): # P(x|z, c)
-        '''
-        z: (bs, latent_size)
-        c: (bs, class_size)
-        '''
-        inputs = torch.cat([z, c.view(-1, 128*128)], 1) # (bs, latent_size+class_size)
-        h3 = self.elu(self.fc3(inputs))
-        return self.sigmoid(self.fc4(h3))
-
-    def forward(self, x, c):
-        mu, logvar = self.encode(x.view(-1, 128*128), c.view(-1, 128*128))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z, c), mu, logvar
-
-# create a CVAE model
-model = CVAE(128*128, latent_size, 128*128).to(device)
-
+# =========== Load CVAE model ============
+model = CVAE(image_size, latent_size, image_size).to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-# Reconstruction + KL divergence losses summed over all elements and batch
+
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 128*128), reduction='sum')
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, image_size), reduction='sum')
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
@@ -135,35 +92,39 @@ def test(epoch):
             data, lattice = data.to(device), lattice.to(device)
             recon_batch, mu, logvar = model(data, lattice)
             test_loss += loss_function(recon_batch, data, mu, logvar).detach().cpu().numpy()
-            if i == 0:
-                n = min(data.size(0), 5)
-                comparison = torch.cat([data.view(-1, 1, 128, 128)[:n],
-                                      recon_batch.view(-1, 1, 128, 128)[:n]])
-                save_image(comparison.cpu(),
-                         'vae/results/reconstruction_' + str(epoch) + '.png', nrow=n)
+            # if i == 0:
+            #     n = min(data.size(0), 5)
+            #     comparison = torch.cat([data.view(-1, 1, 128, 128)[:n],
+            #                           recon_batch.view(-1, 1, 128, 128)[:n]])
+            #     save_image(comparison.cpu(),
+    #                      'vae/results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
-for epoch in range(1, epochs + 1):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            for i, (data, lattice) in enumerate(test_loader):
-                if i == 1:
-                    break
+if __name__ == "__main__":
+    logging.basicConfig(filename='results.log')
 
-                data, lattice = data.to(device), lattice.to(device)
-                lattices = lattice[:8]
-                felix_patterns = data[:8]
+    for epoch in range(1, epochs + 1):
+            train(epoch)
+            test(epoch)
+            with torch.no_grad():
+                pass
+            # for i, (data, lattice) in enumerate(test_loader):
+            #     if i == 1:
+            #         break
 
-                sample = torch.randn(1, 20).to(device)
-                prediction_patterns = torch.cat([model.decode(sample, l).to(device) for l in lattices])
+            #     data, lattice = data.to(device), lattice.to(device)
+            #     lattices = lattice[:8]
+            #     felix_patterns = data[:8]
 
-                comparison = torch.cat([lattices.view(-1, 1, 128, 128)[:8],
-                                        felix_patterns.view(-1, 1, 128, 128)[:8],
-                                        prediction_patterns.view(-1, 1, 128, 128)[:8]])
+            #     sample = torch.randn(1, 20).to(device)
+            #     prediction_patterns = torch.cat([model.decode(sample, l).to(device) for l in lattices])
 
-                save_image(comparison.cpu(),
-                        'vae/results/sample_' + str(epoch) + '.png')
+            #     comparison = torch.cat([lattices.view(-1, 1, 128, 128)[:8],
+            #                             felix_patterns.view(-1, 1, 128, 128)[:8],
+            #                             prediction_patterns.view(-1, 1, 128, 128)[:8]])
+
+            #     save_image(comparison.cpu(),
+            #             'vae/results/sample_' + str(epoch) + '.png')
