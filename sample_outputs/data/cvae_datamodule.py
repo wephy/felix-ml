caa2c9d13ab914026ce5197a545602d80dc68c5f
unknown <xexpozure@gmail.com>
2023-07-13 00:36:52 +0100

    Conv - Deconv model working!

---
+++ sample_outputs/data/cvae_datamodule.py
@@ -0,0 +1,146 @@
+from typing import Any, Dict, Optional, Tuple
+
+import os
+import torch
+# from skimage import io
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision.transforms import transforms
+import numpy as np
+
+
+class CVAE(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(os.listdir(self.data_dir))
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        folder = os.listdir(self.data_dir)[idx]
+        # lattice = io.imread(os.path.join(self.data_dir, folder, "Input.png"))
+        # diffraction = io.imread(os.path.join(self.data_dir, folder, "Output.png"))
+        input_img = np.clip(np.load(os.path.join(self.data_dir, folder, "Input.npy")), 0.0, 1.0)
+        output_img = np.clip(np.load(os.path.join(self.data_dir, folder, "Output.npy")), 0.0, 1.0)
+
+        # if self.transform:
+        #     diffraction = self.transform(diffraction).view(128, 128)
+        #     lattice = self.transform(lattice).view(128, 128)
+
+        return torch.from_numpy(output_img).float().clone().detach(), torch.from_numpy(input_img).float().clone().detach()
+
+
+class CVAEDataModule(LightningDataModule):
+    """Example of LightningDataModule for CVAE dataset.
+
+    A DataModule implements 6 key methods:
+        def prepare_data(self):
+            # things to do on 1 GPU/TPU (not on every GPU/TPU in DDP)
+            # download data, pre-process, split, save to disk, etc...
+        def setup(self, stage):
+            # things to do on every process in DDP
+            # load data, set variables, etc...
+        def train_dataloader(self):
+            # return train dataloader
+        def val_dataloader(self):
+            # return validation dataloader
+        def test_dataloader(self):
+            # return test dataloader
+        def teardown(self):
+            # called on every process in DDP
+            # clean up after fit or test
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://lightning.ai/docs/pytorch/latest/data/datamodule.html
+    """
+    def __init__(
+        self,
+        data_dir: str = "data/CVAE/",
+        train_val_test_split: Tuple[int, int, int] = (9_205, 1_000, 2_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), ]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+        
+        self.data_dir = data_dir
+    # @property
+    # def num_classes(self):
+    #     return 10
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
+        careful not to execute things like random split twice!
+        """
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            dataset = CVAE(self.data_dir, transform=self.transforms)
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None):
+        """Clean up after fit or test."""
+        pass
+
+    def state_dict(self):
+        """Extra things to save to checkpoint."""
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        """Things to do when loading checkpoint."""
+        pass
+
+
+if __name__ == "__main__":
+    _ = CVAEDataModule()
