5a6b206a486715f1d0448483aa2049a959e642ea
unknown <xexpozure@gmail.com>
2023-08-27 00:36:32 +0100

    update script

--- scripts/avon.sh
+++ scripts/avon.sh
@@ -2,7 +2,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
+#SBATCH --mem-per-cpu=3850
+#SBATCH --gres=gpu:ampere_a100:1
 #SBATCH --partition=gpu
+#SBATCH --time=48:00:00
 #SBATCH --acount=su007-rr-gpu
 
 module restore felix-ml
