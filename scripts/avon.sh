49fe9d7ba1f55bec04ee6cebcb5625f50d6d039b
unknown <xexpozure@gmail.com>
2023-09-04 20:18:52 +0100

    script fix

--- scripts/avon.sh
+++ scripts/avon.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --gres=gpu:ampere_a100:1
+#SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --time=48:00:00
 
