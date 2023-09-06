76c35233b19eefb3343a3afcbaa21283073da696
unknown <xexpozure@gmail.com>
2023-09-06 15:30:50 +0100

    cpu

--- scripts/F_m_-3_m.sh
+++ scripts/F_m_-3_m.sh
@@ -3,9 +3,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
-#SBATCH --time=20:00:00
+#SBATCH --partition=cpu
+#SBATCH --time=30:00:00
 
 module restore felix-ml
 
