3b878f1f3611e25980c94aa6b4400a6885bd43a8
unknown <xexpozure@gmail.com>
2023-09-07 00:25:22 +0100

    seed 3 script

--- scripts/avon_24.sh
+++ scripts/avon_24.sh
@@ -3,9 +3,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
-#SBATCH --time=28:00:00
+#SBATCH --partition=compute
+#SBATCH --time=30:00:00
 
 module restore felix-ml
 
