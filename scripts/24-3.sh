52b9a7eced217945496f377b4f17ab09a292352d
unknown <xexpozure@gmail.com>
2023-09-07 11:36:48 +0100

    Seed testing

--- scripts/24-3.sh
+++ scripts/24-3.sh
@@ -3,10 +3,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
-#SBATCH --time=28:00:00
+#SBATCH --partition=compute
+#SBATCH --time=10:00:00
 
 module restore felix-ml
 
-srun python src/train.py experiment=avon_32
+srun python src/train.py experiment=avon_24-3
