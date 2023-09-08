00a2e083efece456d8f28e60c8448e05701e89ba
unknown <xexpozure@gmail.com>
2023-09-08 23:44:14 +0100

    scripts added

---
+++ scripts/P_-4_3_n.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=42
+#SBATCH --mem-per-cpu=3850
+#SBATCH --partition=compute
+#SBATCH --time=30:00:00
+
+module restore felix-ml
+
+srun python src/train.py experiment=P_-4_3_n
